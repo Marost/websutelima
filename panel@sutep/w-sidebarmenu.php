bd00e3acc3bfe2a6ff9b98a4966b3c406080d1fd
MARCO LOPEZ <mlopez@virtualsip.net>
2013-07-29 23:54:53 -0500

    nuevas opciones en menu de administrador

--- panel@sutep/w-sidebarmenu.php
+++ panel@sutep/w-sidebarmenu.php
@@ -9,8 +9,9 @@
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/columnistas/lista.php" title="" ><span class="icos-frames"></span>Columnistas</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php" title="" ><span class="icos-frames"></span>Galería de Fotos</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/portada/lista.php" title=""><span class="icos-frames"></span>Edición Impresa</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php" title=""><span class="icos-frames"></span>Videos</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title=""><span class="icos-frames"></span>Usuarios</a></li>
     </ul>
 
