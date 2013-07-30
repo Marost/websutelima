36be46cfab0e0680da023ff26677a62584477774
MARCO LOPEZ <mlopez@virtualsip.net>
2013-07-29 23:56:26 -0500

    opciones de galeria de fotos, videos y portada en sidebar

--- wg-sidebar.php
+++ wg-sidebar.php
@@ -3,13 +3,11 @@
 include("libs/ssdtube/SSDTube.php");
 
 //EDICION IMPRESA
-/*
-$rst_edimpresa=mysql_query("SELECT * FROM stp_portada WHERE fecha_publicacion<='$fechaActual' ORDER BY id DESC", $conexion);
+$rst_edimpresa=mysql_query("SELECT * FROM stp_portada WHERE fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC", $conexion);
 $fila_edimpresa=mysql_fetch_array($rst_edimpresa);
-$edimpresa_numero=$fila_edimpresa["titulo"];
-$edimpresa_nombre=$fila_edimpresa["nombre_edicion"];
+$edimpresa_titulo=$fila_edimpresa["titulo"];
+$edimpresa_edicion=$fila_edimpresa["num_edicion"];
 $edimpresa_imagen=$fila_edimpresa["imagen"];
-*/
 
 //CARTAS
 $rst_cartas=mysql_query("SELECT * FROM stp_cartas WHERE estado='A' AND fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT 5", $conexion);
@@ -17,21 +15,11 @@ $rst_cartas=mysql_query("SELECT * FROM stp_cartas WHERE estado='A' AND fecha_pub
 //SALUDOS
 $rst_saludos=mysql_query("SELECT * FROM stp_saludos WHERE id>0 AND estado_saludo='A' ORDER BY fecha DESC LIMIT 12", $conexion);
 
-//VIDEO TITULO
-$rst_videos_sup=mysql_query("SELECT * FROM stp_videos WHERE id>0 AND fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT 3;", $conexion);
-$num_videos_sup=mysql_num_rows($rst_videos_sup);
-
-//VIDEO TITULO
-$rst_videos_inf=mysql_query("SELECT * FROM stp_videos WHERE id>0 AND fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT 3;", $conexion);
+//VIDEOS
+$rst_videos=mysql_query("SELECT * FROM stp_videos WHERE fecha_publicacion<='$fechaActual' AND publicar=1 ORDER BY fecha_publicacion DESC;", $conexion);
 
 //GALERIA PRINCIPAL
-$rst_galeria_prin=mysql_query("SELECT * FROM stp_galeria WHERE id>0 ORDER BY id DESC;", $conexion);
-
-//FOTOS DE GALERIA
-function fotoGaleria($idgaleria, $conexion){
-    $rst_query=mysql_query("SELECT * FROM stp_galeria_slide WHERE id>0 AND noticia=$idgaleria ORDER BY orden ASC;", $conexion);
-    return $fila_query=mysql_fetch_array($rst_query);
-}
+$rst_galeria_prin=mysql_query("SELECT * FROM stp_galeria WHERE fecha_publicacion<='$fechaActual' AND publicar=1 ORDER BY fecha_publicacion DESC;", $conexion);
 
 ?>
 <div id="section_sidebar">
@@ -42,8 +30,8 @@ function fotoGaleria($idgaleria, $conexion){
         <div class="scsdbi_cabecera">La Voz del Magisterio</div>
         
         <div class="scsdbi_contenido scsdbic_fdBlanco scsdbic_txtCentro">
-    		<a href="/revista/<?php echo $edimpresa_numero; ?>/index.html" target="_blank">
-                <img src="imagenes/revista/<?php echo $edimpresa_imagen; ?>" width="239" height="320" alt="Portada" title="<?php echo $edimpresa_nombre; ?>">
+    		<a href="/revista/<?php echo $edimpresa_edicion; ?>/index.html" target="_blank">
+                <img src="imagenes/revista/<?php echo $edimpresa_imagen; ?>" width="270" alt="Portada" title="<?php echo $edimpresa_titulo; ?>">
             </a>
         </div>
         
@@ -56,16 +44,38 @@ function fotoGaleria($idgaleria, $conexion){
       	<div class="scsdbi_cabecera">GALERÍA DE IMAGENES</div>
         
       	<div class="scsdbi_contenido scsdbic_ancho290 scsdbic_pTop0 scsdbic_pLeft0 scsdbic_pBottom0 scsdbic_pRight0">
-        	<div id="pgaleria" class="svwp">
-                <ul>
-                    <?php while($fila_galeria_prin=mysql_fetch_array($rst_galeria_prin)){
-                            $fotoGaleria=fotoGaleria($fila_galeria_prin["id"], $conexion);
-                    ?>
-                    <li><a href="galeria/<?php echo $fila_galeria_prin["id"]."-".$fila_galeria_prin["url"]; ?>" title="<?php echo $fila_galeria_prin["titulo"]; ?>">
-                        <img width="290" height="210" src="imagenes/galeria/<?php echo $fotoGaleria["carpeta"]."thumb/".$fotoGaleria["imagen"]; ?>" alt="<?php echo $fila_galeria_prin["titulo"]; ?>" /></a></li>
-                    <?php } ?>
-                </ul>
-            </div><!-- GALERIA WEB GALERIA -->
+
+            <a href="javascript:;" id="slider-prev">Prev</a>
+            <a href="javascript:;" id="slider-next">Next</a>
+
+        	<div class="galeria-sidebar royalSlider rsDefault visibleNearby">
+                
+                <?php while($fila_galeria_prin=mysql_fetch_array($rst_galeria_prin)){
+                        $galeriPrin_id=$fila_galeria_prin["id"];
+                        $galeriPrin_url=$fila_galeria_prin["url"];
+                        $galeriPrin_titulo=$fila_galeria_prin["titulo"];
+                        $galeriPrin_urlWeb=$web."galeria/".$galeriPrin_id."-".$galeriPrin_url;
+                        
+                        //SELECCION DE IMAGEN
+                        $rst_galImg=mysql_query("SELECT * FROM stp_galeria_slide WHERE noticia=$galeriPrin_id AND orden=0", $conexion);
+                        $fila_galImg=mysql_fetch_array($rst_galImg);
+
+                        $galeriPrin_imagen=$fila_galImg["imagen"];
+                        $galeriPrin_imagen_carpeta=$fila_galImg["imagen_carpeta"];
+                ?>
+
+                <div>
+                    <img class="rsImg" src="imagenes/galeria/<?php echo $galeriPrin_imagen_carpeta."".$galeriPrin_imagen; ?>" alt="<?php echo $galeriPrin_titulo; ?>" />
+                    <figure class="rsCaption">
+                        <a href="<?php echo $galeriPrin_urlWeb; ?>" title="">
+                            <?php echo $galeriPrin_titulo; ?>
+                        </a>
+                    </figure>
+                </div>
+                
+                <?php } ?>
+
+            </div>
         </div>
         
     </div>
@@ -77,59 +87,29 @@ function fotoGaleria($idgaleria, $conexion){
       	<div class="scsdbi_cabecera">VIDEOS</div>
         
    	  	<div class="scsdbi_contenido scsdbic_ancho290 scsdbic_pTop0 scsdbic_pLeft0 scsdbic_pBottom0 scsdbic_pRight0">
+
+            <a href="javascript:;" id="slider-prev">Prev</a>
+            <a href="javascript:;" id="slider-next">Next</a>
             
-            <div id="scsdbic_video_select">
-				<?php while($fila_videos_sup=mysql_fetch_array($rst_videos_sup)){ ?>
-                    <div>
-                        <div class="pvids_imagen">
-                        <style type="text/css">
-                            .player{ width:290px; height:193px; float:left; cursor:pointer;}
-                            .player img{ margin-left:105px; margin-top:56px; }
-                        </style>
-                        <?php echo 
-                            tipoVideo($fila_videos_sup["tipo_video"], 
-                            $fila_videos_sup["carpeta_video"],
-                            $fila_videos_sup["video"],
-                            $fila_videos_sup["imagen"],
-                            $fila_videos_sup["carpeta_imagen"]."thumb/",
-                            $fila_videos_sup["id"], 290, 193, $web) ?>
-                          </div>
-                          <div class="pvids_descripcion">
-                            <p><?php echo stripslashes($fila_videos_sup["titulo"]); ?></p>
-                          </div>
-                    </div><!-- PANEL VIDEO ITEM LISTA-->
-                <?php } ?>
-            </div>
-            
-          <div id="scsdbic_video_items">
-              
-              <ul>
-				<?php while($fila_videos_inf=mysql_fetch_array($rst_videos_inf)){
-                        $video_inf=$fila_videos_inf["video"];
-                        $urlyoutube="http://www.youtube.com/watch?v=".$video_inf;
-						$youtube = new SSDTube();
-						$youtube->identify($urlyoutube, true);
-                ?>
-                <li>
-                    <div class="pvidii_imagen">
-                        <?php if($fila_videos_inf["imagen"]==""){ ?>
-                        <img src="<?php echo $youtube->thumbnail_1_url; ?>" width="85" height="70" alt="<?php echo $fila_videos_inf["titulo"]; ?>"/>
-                        
-                        <?php }else{ ?>
-                      	<img src="imagenes/upload/<?php echo $fila_videos_inf["carpeta_imagen"]."thumb/".$fila_videos_inf["imagen"] ?>" width="85" height="70" alt="<?php echo $fila_videos_inf["titulo"]; ?>" />
-                        <?php } ?>
-                  </div>
-                        
-                  <div class="pvidii_contenido">
-                        <p><?php echo $fila_videos_inf["titulo"]; ?></p>
-                  </div>
-                </li>
-                <?php } ?>
+            <div class="galeria-sidebar royalSlider rsDefault visibleNearby" >
                 
-            </ul>
-              
-              
-         </div>
+                <?php while($fila_videos=mysql_fetch_array($rst_videos)){
+                        $galeriPrin_titulo=$fila_videos["titulo"];
+                        $galeriPrin_video=$fila_videos["video"];
+                        $galeriPrin_imagen=$fila_videos["imagen"];
+                        $galeriPrin_imagen_carpeta=$fila_videos["imagen_carpeta"];                        
+                ?>
+
+                <div>
+                    <img class="rsImg" src="imagenes/upload/<?php echo $galeriPrin_imagen_carpeta."".$galeriPrin_imagen; ?>" 
+                    alt="<?php echo $galeriPrin_titulo; ?>"
+                    data-rsVideo="http://www.youtube.com/watch?v=<?php echo $galeriPrin_video; ?>"/>
+                    <figure class="rsCaption"><?php echo $galeriPrin_titulo; ?></figure>
+                </div>
+                
+                <?php } ?>
+
+            </div>
             
       	</div>
         
