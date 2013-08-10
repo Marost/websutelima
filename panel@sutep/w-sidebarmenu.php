dc1cba2395b436a5b5176c6097bdad04ca25b861
MARCO LOPEZ <mlopez@virtualsip.net>
2013-08-10 01:09:39 -0500

    opcion institucional en administrador

--- panel@sutep/w-sidebarmenu.php
+++ panel@sutep/w-sidebarmenu.php
@@ -6,6 +6,7 @@
     
     <!-- Sidebar subnav -->
     <ul class="subNav">
+        <li><a href="<?php echo $url_admin; ?>paginas/institucional/lista.php" title=""><span class="icos-frames"></span>Institucional</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
