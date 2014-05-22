68508372ff9cad5c9cd30e9b0c7c27a1e588aaef
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-05-22 08:33:00 -0500

    Administrador: creacion de Secciones para Institucionales y Borrador

---
+++ panel@sutep/paginas/secciones/s-editar.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include("../../conexion/conexion.php");
+include("../../conexion/funciones.php");
+require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
+
+//DECLARACION DE VARIABLES
+$nota_id=$_REQUEST["id"];
+$nombre=$_POST["nombre"];
+$url=getUrlAmigable(eliminarTextoURL($nombre));
+$contenido=$_POST["contenido"];
+$categoria=$_POST["categoria"];
+$tipo_noticia=$_POST["tipo_noticia"];
+$tags=$_POST["tags"];
+
+//FECHA Y HORA
+$pub_fecha=$_POST["pub_fecha"];
+$pub_hora=$_POST["pub_hora"];
+$fecha_publicacion=$pub_fecha." ".$pub_hora;
+
+//PUBLICAR
+if ($_POST["publicar"]<>""){ $publicar=$_POST["publicar"]; }else{ $publicar=0; }
+
+//INSERTANDO DATOS
+$rst_guardar=mysql_query("UPDATE ".$tabla_suf."_secciones SET url='$url', titulo='".htmlspecialchars($nombre)."', 
+	contenido='$contenido', 
+	fecha_publicacion='$fecha_publicacion', 
+	publicar=$publicar WHERE id=$nota_id;", $conexion);
+
+if (mysql_errno()!=0){
+	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
+	mysql_close($conexion);
+	header("Location:lista.php?msj=er");
+} else {
+	mysql_close($conexion);
+	header("Location:lista.php?msj=ok");
+}
+
+?>
