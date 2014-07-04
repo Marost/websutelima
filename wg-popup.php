d79dcb9802f00da0bf1ac6804df8bb9535f2e352
Marco Lopez <mlopez@marostdevelopers.com>
2014-07-03 21:55:47 -0500

    Home: WG de popup

---
+++ wg-popup.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/blitzer/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+<script>
+var jPopUp=jQuery.noConflict();
+jPopUp(function() {
+jPopUp( "#dialog-message" ).dialog({
+	modal: true,
+	width: "400px"
+});
+});
+</script>
+<div id="dialog-message" title="Somos los Maestros">
+	<p>Somos los maestros del Per&uacute;,</p>
+	<p>juntos como siempre en la misi&oacute;n</p>
+	<p>de educar a nuestra juventud</p>
+	<p>y forjar una nueva naci&oacute;n.</p>
+	<p>&nbsp;</p>
+	<p>Somos los maestros del Per&uacute;,</p>
+	<p>vamos por donde la patria va,</p>
+	<p>orgullosos de la gratitud</p>
+	<p>y el cari&ntilde;o que el pueblo nos da.</p>
+	<p>&nbsp;</p>
+	<p>Somos los obreros del saber,</p>
+	<p>Maestros con honor primero,</p>
+	<p>nunca hemos temblado ante el poder</p>
+	<p>ni nos acomplej&oacute; el dinero.</p>
+	<p>&nbsp;</p>
+	<p>Somos los que hemos querido ser,</p>
+	<p>Maestros de ayer hoy y siempre</p>
+	<p>y si volvi&eacute;ramos a nacer</p>
+	<p>lo ser&iacute;amos nuevamente.</p>
+	<p>&nbsp;</p>
+	<p>Somos los que hemos querido ser,</p>
+	<p>Maestros de ayer hoy y siempre</p>
+	<p>y si volvi&eacute;ramos a nacer</p>
+	<p>lo ser&iacute;amos nuevamente.</p>
+	<p>&nbsp;</p>
+	<p>Somos los maestros del Per&uacute;</p>
+  	<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/157223670&amp;color=ff0000&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false"></iframe>
+</div>
